17a18cdb6308105aa33e6400c48a1e9066185275
Andreas Kutscheid <sco@shaula.de>
2024-02-25 20:55:57 +0100

    draw triangles with culling

--- main.gd
+++ main.gd
@@ -5,6 +5,13 @@ extends Node2D
 # size of SubViewport
 const _screenSize: Vector2i = Vector2i(320, 256)
 
+var _triangles: Array[int] = [
+	# bottom
+	-50, 0, 0, 50, 0, 0,
+	50, 0, 0, 0, 0, 50,
+	0, 0, 50, -50, 0, 0,
+]
+
 var _lines: Array[int] = [
 	# RUMPF
 	-50,  0,  0, 50,  0, 40,
@@ -83,12 +90,13 @@ func _ready():
 	_projection = _camera.get_camera_projection()
 
 
-func _draw():
+func draw_lines():
+	var array: Array[int] = _triangles
 	var index = 0
-	while index < _lines.size():
-		var p1: Vector3 = nextPnt(index, _lines)
+	while index < array.size():
+		var p1: Vector3 = nextPnt(index, array)
 		index += 3
-		var p2: Vector3 = nextPnt(index, _lines)
+		var p2: Vector3 = nextPnt(index, array)
 		index += 3
 		
 		p1 = rotatePnt(p1, _angles)
@@ -97,7 +105,53 @@ func _draw():
 		var p1_2i: Vector2i = projectAndMovePnt(p1, _projection, _screenSize)
 		var p2_2i: Vector2i = projectAndMovePnt(p2, _projection, _screenSize)
 		
-		draw_line(p1_2i, p2_2i, Color.FOREST_GREEN)
+		draw_line(p1_2i, p2_2i, Color.AZURE)
+
+
+func isTriangleVisible(p1: Vector2, p2: Vector2, p3: Vector2) -> bool:
+	# straight-forward:
+	#
+	# 	var v1: Vector3 = p2 - p1
+	# 	var v2: Vector3 = p3 - p2
+	# 	return v1.cross(v2).z > 0
+	
+	# optimized
+	#
+	# 	cross-z: v1.x * v2.y - v1.y * v2.x
+	
+	var v1: Vector2 = p2 - p1
+	var v2: Vector2 = p3 - p2
+	return v1.x * v2.y - v1.y * v2.x > 0
+	
+
+func draw_triangles():
+	var array: Array[int] = _triangles
+	var index = 0
+	while index < array.size():
+		var p1: Vector3 = nextPnt(index, array)
+		index += 6
+		var p2: Vector3 = nextPnt(index, array)
+		index += 6
+		var p3: Vector3 = nextPnt(index, array)
+		index += 6
+		
+		p1 = rotatePnt(p1, _angles)
+		p2 = rotatePnt(p2, _angles)
+		p3 = rotatePnt(p3, _angles)
+		
+		if not isTriangleVisible(Vector2(p1.x, p1.y), Vector2(p2.x, p2.y), Vector2(p3.x, p3.y)):
+			continue
+		
+		var p1_2i: Vector2i = projectAndMovePnt(p1, _projection, _screenSize)
+		var p2_2i: Vector2i = projectAndMovePnt(p2, _projection, _screenSize)
+		var p3_2i: Vector2i = projectAndMovePnt(p3, _projection, _screenSize)
+		
+		draw_colored_polygon([p1_2i, p2_2i, p3_2i], Color.FOREST_GREEN)
+
+
+func _draw():
+	draw_triangles()
+	draw_lines()
 
 
 func _process(_delta):
